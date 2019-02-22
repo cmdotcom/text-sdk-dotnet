285e0f43cccb7a1142c94200636a470315113523
bg <bas.gijzen@cm.nl>
2019-02-22 13:23:37 +0100

    -Use .NET style naming for data models - Adding Message and used classes as public class, source for comments is: https://docs.cmtelecom.com/business-messaging/v1.0 - Adding MessageBuilder to be able to build messages easily

---
+++ CM.Text/BusinessMessaging/Model/Recipient.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace CM.Text.BusinessMessaging.Model {
+    /// <summary>
+    /// A destination mobile number.
+    /// </summary>
+    public class Recipient {
+        /// <summary>
+        /// This value should be in international format.
+        /// A single mobile number per request. Example: '00447911123456'
+        /// </summary>
+        [JsonProperty("number")]
+        public string Number { get; set; }
+    }
+}
