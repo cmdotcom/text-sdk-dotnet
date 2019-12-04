50ea4e61182c671fea0d45b554b383e72451ea0c
Michel de Wit <michel.de.wit@recruitnow.nl>
2019-12-04 14:30:17 +0100

    Removed unused directives

--- CM.Text/BusinessMessaging/Model/MultiChannel/TemplateMessageContent.cs
+++ CM.Text/BusinessMessaging/Model/MultiChannel/TemplateMessageContent.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 using Newtonsoft.Json;
 
 namespace CM.Text.BusinessMessaging.Model.MultiChannel
