95bcad971a69b24180503063af1efcb9a2b1b70e
Robin Voogt <rv@cm.nl>
2021-07-06 14:34:38 +0200

    Added Instagram Channel

--- CM.Text/TextClient.cs
+++ CM.Text/TextClient.cs
@@ -22,14 +22,16 @@ namespace CM.Text
         ///     For Twitter: use the Twitter Snowflake ID of the account you want to use as sender.<br/>
         ///     For MobilePush: use the app key of the account you want to use as sender.<br/>
         ///     For Facebook Messenger: use the Facebook Page ID of the account you want to use as sender.<br/>
-        ///     For Google Business Messages: use the Google Business Messages agent ID of the account you want to use as sender (without dashes).
+        ///     For Google Business Messages: use the Google Business Messages agent ID of the account you want to use as sender (without dashes).<br/>
+        ///     For Instagram: use the Instagram Account ID of the account you want to use as sender.
         /// </param>
         /// <param name="to">
         ///     These are the destination mobile numbers. Restrictions: this value should be in international format.
         ///     Example: '00447911123456'.<br/>
         ///     For Twitter: use the Twitter Snowflake ID.<br/>
         ///     For Facebook Messenger: use the Facebook Page Scoped User ID (PSID).<br/>
-        ///     For Google Business Messages: use the Google Business Messages conversation ID (without dashes).
+        ///     For Google Business Messages: use the Google Business Messages conversation ID (without dashes).<br/>
+        ///     For Instagram: use the Instagram Scoped User ID (IGSID).
         /// </param>
         /// <param name="reference">
         ///     Here you can include your message reference. This information will be returned in a status
@@ -97,14 +99,16 @@ namespace CM.Text
         ///     For Twitter: use the Twitter Snowflake ID of the account you want to use as sender.<br/>
         ///     For MobilePush: use the app key of the account you want to use as sender.<br/>
         ///     For Facebook Messenger: use the Facebook Page ID of the account you want to use as sender.<br/>
-        ///     For Google Business Messages: use the Google Business Messages agent ID of the account you want to use as sender (without dashes).
+        ///     For Google Business Messages: use the Google Business Messages agent ID of the account you want to use as sender (without dashes).<br/>
+        ///     For Instagram: use the Instagram Account ID of the account you want to use as sender.
         /// </param>
         /// <param name="to">
         ///     These are the destination mobile numbers. Restrictions: this value should be in international format.
         ///     Example: '00447911123456'.<br/>
         ///     For Twitter: use the Twitter Snowflake ID.<br/>
         ///     For Facebook Messenger: use the Facebook Page Scoped User ID (PSID).<br/>
-        ///     For Google Business Messages: use the Google Business Messages conversation ID (without dashes).
+        ///     For Google Business Messages: use the Google Business Messages conversation ID (without dashes).<br/>
+        ///     For Instagram: use the Instagram Scoped User ID (IGSID).
         /// </param>
         /// <param name="reference">
         ///     Here you can include your message reference. This information will be returned in a status
