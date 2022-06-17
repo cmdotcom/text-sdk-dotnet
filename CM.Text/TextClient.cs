6587686cdcf0dbe18c6d1eecbf79e6d42acc48f7
Joep Beusenberg <joep.beusenberg@cm.com>
2022-06-17 17:40:33 +0400

    Add Telegram

--- CM.Text/TextClient.cs
+++ CM.Text/TextClient.cs
@@ -21,13 +21,13 @@ namespace CM.Text
         /// </summary>
         /// <param name="messageText">The message text.</param>
         /// <param name="from">
-        ///     This is the sender name. The maximum length is 11 alphanumerical characters or 16 digits. Example:
-        ///     'MyCompany'.<br/>
+        ///     This is the sender name. The maximum length is 11 alphanumerical characters or 16 digits. Example: 'MyCompany'.<br/>
         ///     For Twitter: use the Twitter Snowflake ID of the account you want to use as sender.<br/>
         ///     For MobilePush: use the app key of the account you want to use as sender.<br/>
         ///     For Facebook Messenger: use the Facebook Page ID of the account you want to use as sender.<br/>
         ///     For Google Business Messages: use the Google Business Messages agent ID of the account you want to use as sender (without dashes).<br/>
-        ///     For Instagram: use the Instagram Account ID of the account you want to use as sender.
+        ///     For Instagram: use the Instagram Account ID of the account you want to use as sender.<br/>
+        ///     For Telegram: use the Telegram Bot ID of the account you want to use as sender.
         /// </param>
         /// <param name="to">
         ///     These are the destination mobile numbers. Restrictions: this value should be in international format.
@@ -35,7 +35,8 @@ namespace CM.Text
         ///     For Twitter: use the Twitter Snowflake ID.<br/>
         ///     For Facebook Messenger: use the Facebook Page Scoped User ID (PSID).<br/>
         ///     For Google Business Messages: use the Google Business Messages conversation ID (without dashes).<br/>
-        ///     For Instagram: use the Instagram Scoped User ID (IGSID).
+        ///     For Instagram: use the Instagram Scoped User ID (IGSID).<br/>
+        ///     For Telegram: use the Telegram Chat ID.
         /// </param>
         /// <param name="reference">
         ///     Here you can include your message reference. This information will be returned in a status
