21c81e3f7161298ef76fdce1cc331a6bed74ebb7
Enes <9263360+EnessenE@users.noreply.github.com>
2022-07-04 22:08:35 +0200

    Update BusinessMessagingApi.cs

--- CM.Text/BusinessMessaging/BusinessMessagingApi.cs
+++ CM.Text/BusinessMessaging/BusinessMessagingApi.cs
@@ -75,5 +75,13 @@ namespace CM.Text.BusinessMessaging
                     .ToArray()
             };
         }
+        internal static class Constant
+        {
+            internal const string BusinessMessagingGatewayJsonEndpoint = "https://gw.cmtelecom.com/v1.0/message";
+            internal const string BusinessMessagingGatewayMediaTypeJson = "application/json";
+            internal const string BusinessMessagingBodyTypeAuto = "AUTO";
+            internal const int BusinessMessagingMessagePartsMinDefault = 1;
+            internal const int BusinessMessagingMessagePartsMaxDefault = 8;
+        }
     }
 }
