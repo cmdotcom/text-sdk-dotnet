f08ed17a58225e1c2dac3a34495ba611c70ca9a2
Enes Keric <9263360+EnessenE@users.noreply.github.com>
2022-10-13 08:46:26 +0200

    Update Constant.cs

--- CM.Text/Common/Constant.cs
+++ CM.Text/Common/Constant.cs
@@ -4,7 +4,7 @@
     {
         internal static readonly string TextSdkReference = $"text-sdk-dotnet-{typeof(TextClient).Assembly.GetName().Version}";
 
-        internal static readonly string BusinessMessagingGatewayJsonEndpoint = "http://gateway.staging.messaging.cmgroep.local/v1.0/message";
+        internal const string BusinessMessagingGatewayJsonEndpoint = "https://gw.cmtelecom.com/v1.0/message";
         internal static readonly string BusinessMessagingGatewayMediaTypeJson = "application/json";
         internal static readonly string BusinessMessagingBodyTypeAuto = "AUTO";
         internal static readonly int BusinessMessagingMessagePartsMinDefault = 1;
