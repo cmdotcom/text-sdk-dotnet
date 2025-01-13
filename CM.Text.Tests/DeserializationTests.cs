3e5e8e94bf3e4e7fcf23d083df1e306ddb8b12ce
Martijn Vos <martijn.vos@cm.com>
2025-01-13 14:34:33 +0100

    Test deserializing RichContent when no type discriminator is present

--- CM.Text.Tests/DeserializationTests.cs
+++ CM.Text.Tests/DeserializationTests.cs
@@ -17,6 +17,28 @@ namespace CM.Text.Tests
             var deserializedTextMessage = deserialized as TextMessage;
             deserializedTextMessage!.Text.Should().Be("testing correct deserialization");
         }
+
+        [TestMethod]
+        public void DeserializeTemplateMessageWithTypeTest()
+        {
+            var richContentJson =
+                "{\"conversation\": [{\"template\": {\"whatsapp\": {\"namespace\": \"some_identifier\",\"element_name\": \"image__link\",\"language\": {\"code\": \"en\",\"policy\": \"deterministic\"},\"components\": [{\"type\": \"header\",\"parameters\": [{\"type\": \"image\",\"media\": {\"mediaUri\": \"https://example.com/image.png\",\"mimeType\": \"image/jpeg\"}}]},{\"type\": \"body\",\"parameters\": [{\"type\": \"text\",\"text\": \"testing\"}]},{\"type\": \"button\",\"sub_type\": \"url\",\"index\": \"0\",\"parameters\": [{\"type\": \"text\",\"text\": \"whatsapp\"}]}]}}}]}";
+            var deserialized = JsonSerializer.Deserialize<RichContent>(richContentJson);
+            deserialized.Should().NotBeNull();
+            deserialized!.Conversation.FirstOrDefault().Should().BeOfType<TemplateMessage>();
+        }
+
+        [TestMethod]
+        public void DeserializeTextMessageWithTypeTest()
+        {
+            var richContentJson =
+                "{\"conversation\": [{\"text\": \"testing\"}]}";
+            var deserialized = JsonSerializer.Deserialize<RichContent>(richContentJson);
+            deserialized.Should().NotBeNull();
+            deserialized!.Conversation.FirstOrDefault().Should().BeOfType<TextMessage>();
+
+            var message = deserialized.Conversation.First();
+        }
     }
 }
 
